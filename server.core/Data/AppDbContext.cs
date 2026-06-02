6f02a2fbef516776dad92a3df30a706ff9fe8756
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2026-06-02 09:50:55 -0700

    Create new app schema

--- server.core/Data/AppDbContext.cs
+++ server.core/Data/AppDbContext.cs
@@ -5,5 +5,15 @@ namespace Server.Core.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public const string AppSchema = "app";
+
     public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.HasDefaultSchema(AppSchema);
+        modelBuilder.Entity<User>().ToTable("Users", AppSchema);
+    }
 }
